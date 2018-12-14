4322a78a7bc88100fb2c9d3439f713bf4fc6569f
Emmanuel <e.o.donate@gmail.com>
2018-12-13 17:36:40 -0800

    Added Toggle and form input functionality with hooks

---
+++ src/context.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodosContext = React.createContext({
+    todos: [ 
+        { id: 1, text: "Eat breakfast", complete: false },
+        { id: 2, text: "Go for a run", complete: false },
+        { id: 3, text: "Go to the movies", complete: true }
+    ]
+})
+
+export default TodosContext;
